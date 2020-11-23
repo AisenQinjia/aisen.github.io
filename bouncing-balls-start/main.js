4796f57c79cbb85bb99c9645d287aa611b765b30
艾森 <aisen@shangyoo.com>
2020-11-23 21:21:48 +0800

    evil circle

--- bouncing-balls-start/main.js
+++ bouncing-balls-start/main.js
@@ -6,7 +6,9 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight - 50;
 
-// 生成随机数的函数
+
+let balls = []
+    // 生成随机数的函数
 
 function random(min, max) {
     const num = Math.floor(Math.random() * (max - min)) + min;
@@ -20,15 +22,92 @@ function randomColor() {
         random(0, 255) + ')';
 }
 
-function Ball(x, y, velX, velY, color, size) {
+
+function CirclePrototype(x, y, velX, velY, exist) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.exist = exist;
+}
+CirclePrototype.prototype.testCy = function() {
+
+}
+
+function EvilCircle(x, y, velX, velY, exist) {
+    CirclePrototype.call(this, x, y, velX, velY, exist);
+    this.color = 'white';
+    this.size = 10;
+}
+EvilCircle.prototype = Object.create(CirclePrototype.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+EvilCircle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+EvilCircle.prototype.checkBounds = function() {
+    if ((this.x + this.size) >= width) {
+        this.x = this.x - this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.x = this.x + this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.y = this.y - this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.y = this.y + this.size;
+    }
+}
+EvilCircle.prototype.setControls = function() {
+    window.onkeydown = e => {
+        switch (e.key) {
+            case 'a':
+                this.x -= this.velX;
+                break;
+            case 'd':
+                this.x += this.velX;
+                break;
+            case 'w':
+                this.y -= this.velY;
+                break;
+            case 's':
+                this.y += this.velY;
+                break;
+        }
+    };
+}
+EvilCircle.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+        let ball = balls[j];
+        if (this != balls[j] && ball.exist) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if ((distance < this.size + ball.size)) {
+                ball.exist = false;
+                ball.color = "black"
+                balls.splice(j, 1);
+            }
+        }
+    }
+}
+
+function Ball(x, y, velX, velY, color, size, exist) {
+    CirclePrototype.call(this, x, y, velX, velY, exist);
     this.color = color;
     this.size = size;
     this.collidingBall = new Set();
 }
+
+Ball.prototype = Object.create(CirclePrototype.prototype);
+Ball.prototype.constructor = Ball;
 
 Ball.prototype.draw = function() {
     ctx.beginPath();
@@ -77,7 +156,6 @@ Ball.prototype.collisionDetect = function() {
     }
 }
 
-let balls = []
 
 while (balls.length < 25) {
     let size = random(10, 20);
@@ -87,10 +165,13 @@ while (balls.length < 25) {
         random(-7, 7),
         random(-7, 7),
         randomColor(),
-        size
+        size,
+        true
     );
     balls.push(ball);
 }
+let evilCircle = new EvilCircle(width / 2, height / 2, 20, 20, true);
+evilCircle.setControls();
 
 function loop() {
     ctx.fillStyle = 'rgb(0,0,0,0.25)';
@@ -100,6 +181,9 @@ function loop() {
         balls[i].collisionDetect();
         balls[i].draw();
     }
+    evilCircle.checkBounds();
+    evilCircle.collisionDetect();
+    evilCircle.draw();
     requestAnimationFrame(loop);
 }
 
